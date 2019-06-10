29caa1bfc238fa27c700796b37827d1046151218
Akhand Mishra <akkimysite@gmail.com>
2019-06-10 20:58:00 +0530

    Updates

---
+++ precache-manifest.b48c3ba66b9f76ad11cd11c5ba8ddec6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5271dee1c374b9aafc12f76b6f5da9f2",
+    "url": "/cars/index.html"
+  },
+  {
+    "revision": "f7da9429c233d6d68f37",
+    "url": "/cars/static/css/2.027be111.chunk.css"
+  },
+  {
+    "revision": "fd67a0e0ba38ff015bf8",
+    "url": "/cars/static/css/main.d0874f75.chunk.css"
+  },
+  {
+    "revision": "f7da9429c233d6d68f37",
+    "url": "/cars/static/js/2.006b94cc.chunk.js"
+  },
+  {
+    "revision": "fd67a0e0ba38ff015bf8",
+    "url": "/cars/static/js/main.05a9d846.chunk.js"
+  },
+  {
+    "revision": "f4056a0bb5fd0cab84a6",
+    "url": "/cars/static/js/runtime~main.b66e73c4.js"
+  },
+  {
+    "revision": "9f2131c2974ff6dcfefe82f5727c466d",
+    "url": "/cars/static/media/background.9f2131c2.svg"
+  },
+  {
+    "revision": "7bd9a61f8f1ba09b4d7f957d243470d7",
+    "url": "/cars/static/media/compass.7bd9a61f.svg"
+  },
+  {
+    "revision": "e215c0dce69f8373c6e8c0de20a5f80c",
+    "url": "/cars/static/media/eye.e215c0dc.png"
+  },
+  {
+    "revision": "adb137f0fa13eb025c13d13c727fe7fa",
+    "url": "/cars/static/media/grab.adb137f0.svg"
+  },
+  {
+    "revision": "3acfddd5c09c90a4f089f8d626aebca1",
+    "url": "/cars/static/media/grabbing.3acfddd5.svg"
+  },
+  {
+    "revision": "f37aa8ca72b5548a255c62dab1614437",
+    "url": "/cars/static/media/sprites.f37aa8ca.svg"
+  }
+]);
